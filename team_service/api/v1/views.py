529de9940ee4f2af566712a2123c4d2e7f33c8f4
brunogonzalezlla <brunogllaga@icloud.com>
2022-12-11 14:44:09 +0100

    Add auth token to database and fix errors

--- team_service/api/v1/views.py
+++ team_service/api/v1/views.py
@@ -8,6 +8,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.conf import settings
 
+
 class TeamViewSet(viewsets.ModelViewSet):
   queryset = Team.objects.all()
   serializer_class = TeamSerializer
@@ -15,5 +16,5 @@ class TeamViewSet(viewsets.ModelViewSet):
 class GoogleLogin(SocialLoginView):
     authentication_classes = [] # disable authentication
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:3000"
+    callback_url = "http://localhost:3000/login"
     client_class = OAuth2Client
