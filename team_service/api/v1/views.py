85934ab38ec6dff1cd2d90d606bb6c951ed4af4c
brunogonzalezlla <brunogllaga@icloud.com>
2022-12-16 13:26:02 +0100

    Remove dependency

--- team_service/api/v1/views.py
+++ team_service/api/v1/views.py
@@ -11,7 +11,6 @@ from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 
 
 class TeamViewSet(viewsets.ModelViewSet):
