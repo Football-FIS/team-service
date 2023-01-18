9178036f755c9236529269163a6f9e4dcafa2c32
brunogonzalezlla <brunogllaga@icloud.com>
2023-01-18 22:46:41 +0100

    Added more test

--- team_service/api/v1/tests/integration/test_create_team.py
+++ team_service/api/v1/tests/integration/test_create_team.py
@@ -83,18 +83,127 @@ def test_create_team_with_invalid_param(client):
         'address': 'C. Sevilla Fútbol Club',
         'coach_name': 'Jorge Sampaoli',
         'stadium_name': 'Estadio Ramón Sánchez-Pizjuán',
+        'capacity_stadium': '1',
+        'president_name': 'José Castro Carmona',
+        'league_name': 'La Liga',
+        'latitude': '75.021313',
+        'longuitude': '56.021313',
+        'plan_type': 'POS',
+        'matches_month_created': 0
+    }
+
+    response = client.post(request_url, data=json_team, format='json')
+
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_create_team_with_invalid_date(client):
+    """
+    Check create method with invalid param.
+    """
+    json_team = {
+        'name': 'Sevilla FC',
+        'country': 'Spain',
+        'state': 'Sevilla',
+        'city': 'Sevilla',
+        'address': 'C. Sevilla Fútbol Club',
+        'coach_name': 'Jorge Sampaoli',
+        'stadium_name': 'Estadio Ramón Sánchez-Pizjuán',
         'capacity_stadium': 'bad value',
         'president_name': 'José Castro Carmona',
         'league_name': 'La Liga',
         'latitude': '75.021313',
         'longuitude': '56.021313',
         'plan_type': 'PRE',
+        'expiration': '2222222',
         'matches_month_created': 0
     }
 
     response = client.post(request_url, data=json_team, format='json')
 
-    expected_response = json_team
-    expected_response['expiration'] = None
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_create_team_with_invalid_capacity(client):
+    """
+    Check create method with invalid param.
+    """
+    json_team = {
+        'name': 'Sevilla FC',
+        'country': 'Spain',
+        'state': 'Sevilla',
+        'city': 'Sevilla',
+        'address': 'C. Sevilla Fútbol Club',
+        'coach_name': 'Jorge Sampaoli',
+        'stadium_name': 'Estadio Ramón Sánchez-Pizjuán',
+        'capacity_stadium': 'bad value',
+        'president_name': 'José Castro Carmona',
+        'league_name': 'La Liga',
+        'latitude': '75.021313',
+        'longuitude': '56.021313',
+        'plan_type': 'PRE',
+        'expiration': '2222222',
+        'matches_month_created': 0
+    }
+
+    response = client.post(request_url, data=json_team, format='json')
+
+    assert response.status_code == 400
+    
+
+@pytest.mark.django_db
+def test_create_team_with_invalid_plan_type(client):
+    """
+    Check create method with invalid param.
+    """
+    json_team = {
+        'name': 'Sevilla FC',
+        'country': 'Spain',
+        'state': 'Sevilla',
+        'city': 'Sevilla',
+        'address': 'C. Sevilla Fútbol Club',
+        'coach_name': 'Jorge Sampaoli',
+        'stadium_name': 'Estadio Ramón Sánchez-Pizjuán',
+        'capacity_stadium': 'bad value',
+        'president_name': 'José Castro Carmona',
+        'league_name': 'La Liga',
+        'latitude': '75.021313',
+        'longuitude': '56.021313',
+        'plan_type': 'PREMIUM',
+        'expiration': '2222222',
+        'matches_month_created': 0
+    }
+
+    response = client.post(request_url, data=json_team, format='json')
+
+    assert response.status_code == 400
+
+   
+@pytest.mark.django_db
+def test_create_team_with_invalid_matches_created(client):
+    """
+    Check create method with invalid param.
+    """
+    json_team = {
+        'name': 'Sevilla FC',
+        'country': 'Spain',
+        'state': 'Sevilla',
+        'city': 'Sevilla',
+        'address': 'C. Sevilla Fútbol Club',
+        'coach_name': 'Jorge Sampaoli',
+        'stadium_name': 'Estadio Ramón Sánchez-Pizjuán',
+        'capacity_stadium': 'bad value',
+        'president_name': 'José Castro Carmona',
+        'league_name': 'La Liga',
+        'latitude': '75.021313',
+        'longuitude': '56.021313',
+        'plan_type': 'PREMIUM',
+        'expiration': '2222222',
+        'matches_month_created': 1.11
+    }
+
+    response = client.post(request_url, data=json_team, format='json')
 
     assert response.status_code == 400
--- team_service/api/v1/tests/integration/test_update_team.py
+++ team_service/api/v1/tests/integration/test_update_team.py
@@ -63,6 +63,87 @@ def test_update_team_with_invalid_param(client):
     json_team = {
         'user': user.id,
         'capacity_stadium': 'bad value',
+        'expiration': '2222222',
+    }
+
+    url = request_url + str(user.id) + '/'
+
+    response = client.put(url, data=json_team, content_type='application/json')
+
+    assert response.status_code == 400
+    
+
+@pytest.mark.django_db
+def test_update_team_with_invalid_date(client):
+    """
+    Check update method with invalid param.
+    """
+    user = BuilderModel().build_user_test()
+    BuilderModel().build_team_test(user)
+
+    json_team = {
+        'user': user.id,
+        'expiration': '2222222',
+    }
+
+    url = request_url + str(user.id) + '/'
+
+    response = client.put(url, data=json_team, content_type='application/json')
+
+    assert response.status_code == 400
+    
+    
+@pytest.mark.django_db
+def test_update_team_with_invalid_capacity(client):
+    """
+    Check update method with invalid param.
+    """
+    user = BuilderModel().build_user_test()
+    BuilderModel().build_team_test(user)
+
+    json_team = {
+        'user': user.id,
+        'capacity_stadium': 'bad value',
+    }
+
+    url = request_url + str(user.id) + '/'
+
+    response = client.put(url, data=json_team, content_type='application/json')
+
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_team_with_invalid_plan_type(client):
+    """
+    Check update method with invalid param.
+    """
+    user = BuilderModel().build_user_test()
+    BuilderModel().build_team_test(user)
+
+    json_team = {
+        'user': user.id,
+        'plan_type': 'PREMIUM',
+    }
+
+    url = request_url + str(user.id) + '/'
+
+    response = client.put(url, data=json_team, content_type='application/json')
+
+    assert response.status_code == 400
+    
+    
+@pytest.mark.django_db
+def test_update_team_with_invalid_matches_created(client):
+    """
+    Check update method with invalid param.
+    """
+    user = BuilderModel().build_user_test()
+    BuilderModel().build_team_test(user)
+
+    json_team = {
+        'user': user.id,
+        'matches_month_created': 1.11
     }
 
     url = request_url + str(user.id) + '/'
